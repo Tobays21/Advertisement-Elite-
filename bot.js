db78f3fd2448f631c27f62804d682128d4ec9bc0
Tobays21 <35052351+Tobays21@users.noreply.github.com>
2018-01-02 22:01:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+client.on('message', message => {
+    if (message.content === 'discord') {
+        message.reply('https://discord.gg/NfCqvFW');
 });
 
 client.on('message', message => {
